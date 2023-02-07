0467ec09cf0c0237f65efe0de5825ad811f85156
Siavash Ebrahimi <ebrahimi.siavash@gmail.com>
2023-02-07 14:56:40 +0400

    Initiate commit on ChatMat React-Native app

---
+++ components/Chat.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+
+export default class Screen2 extends React.Component {
+  componentDidMount() {
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name });
+  }
+  render() {
+    let name = this.props.route.params.name;
+    let color = this.props.route.params.color;
+    return (
+      <View style={{ backgroundColor: color, flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Chat with {name}</Text>
+        <Button
+          title="Back to Start"
+          onPress={() => this.props.navigation.navigate('Start')}
+        />
+      </View>
+    )
+  }
+}
